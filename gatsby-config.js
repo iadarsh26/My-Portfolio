a4b80c6b35c469db120103a20907413d44c38361
Adarsh Singh <83503111+iadarsh26@users.noreply.github.com>
2021-07-10 19:15:56 +0530

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,7 +2,7 @@ module.exports = {
   siteMetadata: {
     title: 'Adarsh Singh',
     description:
-      'This repo contains an example business website that is built with Gatsby, and Netlify CMS.It follows the JAMstack architecture by using Git as a single source of truth, and Netlify for continuous deployment, and CDN distribution.',
+      'Welcome to my portfolio',
   },
   plugins: [
     'gatsby-plugin-react-helmet',
