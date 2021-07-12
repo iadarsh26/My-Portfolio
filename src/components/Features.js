4a5fa346001c05b7a04056b42977c5bcc32eeee4
Adarsh Singh <83503111+iadarsh26@users.noreply.github.com>
2021-07-12 17:46:32 +0530

    Update Features.js

--- src/components/Features.js
+++ src/components/Features.js
@@ -7,6 +7,7 @@ const FeatureGrid = ({ gridItems }) => (
     {gridItems.map((item) => (
       <div key={item.text} className="column is-6">
         <section className="section">
+          <h4><p className='font-weight-bold text-uppercase'>{item.subheading}</p></h4>
           <div className="has-text-centered">
             <div
               style={{
@@ -28,6 +29,7 @@ FeatureGrid.propTypes = {
   gridItems: PropTypes.arrayOf(
     PropTypes.shape({
       image: PropTypes.oneOfType([PropTypes.object, PropTypes.string]),
+      subheading: PropTypes.string,
       text: PropTypes.string,
     })
   ),
