4cb3226518b5ce6ff9393933b8018cebf093d6a6
Adarsh Singh <83503111+iadarsh26@users.noreply.github.com>
2021-07-24 12:16:59 +0530

    Update index.js

--- src/pages/blog/index.js
+++ src/pages/blog/index.js
@@ -16,8 +16,6 @@ export default class BlogIndexPage extends React.Component {
           <h1
             className="has-text-weight-bold is-size-1"
             style={{
-              boxShadow: '0.5rem 0 0 #0a4359, -0.5rem 0 0 #0a4359',
-              backgroundColor: '#0a4359',
               color: 'white',
               padding: '1rem',
             }}
